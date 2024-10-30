134ffeedf835000aacd0a0adb7af963e061984f1
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-31 04:12:08 +0530

    pushhh

--- backend/frontend/src/navbar.jsx
+++ backend/frontend/src/navbar.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
 // import './navbar.css'; // Make sure to create and style this CSS file
 import "./input.css";
 const Navbar = () => {
+//   const [isLogged, setIsLogged] = useState(false);
+
   return (
     <>
       <nav>
         <div className="flex justify-evenly items-stretch ">
           <div className="flex-1 ml-10">
-            <a href="/" className="text-4xl font-semibold capitalize">Tutorite</a>
+            <a href="/" className="text-4xl font-semibold capitalize">
+              Tutorite
+            </a>
           </div>
-          <div className="text-right m-4 w-1/3" id="">
+          <div className=" m-4 w-1/3" id="">
             <div className="flex justify-around items-center space-x-1 text-l">
               <div className="hover:underline hover:text-blue-300">
                 <a href="/home">Home</a>
-                
               </div>
               <div className="hover:underline hover:text-blue-300">
                 <a href="/about">About</a>
@@ -26,8 +31,21 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="bg-black text-right flex-1">
-            <a href="/login">Login</a>
+          <div className="flex-1 justify-end">
+          <FaUserCircle className="text-2xl text-blue-600 hover:opacity-30" />
+
+            {/* {isLogged ? (
+              <>
+                <FaUserCircle className="text-2xl text-blue-600" />
+                <button onClick={handleLogout} className="text-blue-600">
+                  Logout
+                </button>
+              </>
+            ) : (
+              <button onClick={handleLogin} className="text-blue-600">
+                Sign In
+              </button>
+            )} */}
           </div>
         </div>
       </nav>
