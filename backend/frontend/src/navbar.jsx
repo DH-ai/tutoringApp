e7fe9e7f435c08414399a15ebe329c061a30c94c
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-31 03:48:31 +0530

    push

--- backend/frontend/src/navbar.jsx
+++ backend/frontend/src/navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
     <>
       <nav>
         <div className="flex justify-evenly items-stretch ">
-          <div className="justify-self-start">
+          <div className="flex-1 ml-10">
             <a href="/" className="text-4xl font-semibold capitalize">Tutorite</a>
           </div>
-          <div className="text-right m-4" id="">
-            <div className="flex justify-between items-center space-x-8 hover:underline">
+          <div className="text-right m-4 w-1/3" id="">
+            <div className="flex justify-around items-center space-x-1 font-sans">
               <div className="">
                 <a href="/home">Home</a>
                 
@@ -26,7 +26,7 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="bg-black text-right">
+          <div className="bg-black text-right flex-1">
             <a href="/login">Login</a>
           </div>
         </div>
