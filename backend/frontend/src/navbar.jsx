3d508abfdf4832c29774882161ed891e2ef29c3c
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-31 03:13:25 +0530

    working on ui

--- backend/frontend/src/navbar.jsx
+++ backend/frontend/src/navbar.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React from "react";
 // import './navbar.css'; // Make sure to create and style this CSS file
-import  './output.css';
+import "./output.css";
 const Navbar = () => {
-    return (
-        <>
+  return (
+    <>
+      <nav >
+        <div className="flex justify-around items-baseline bg-slate-100">
+          <div>
 
-        <nav className="bg-blue-300">
-            <div className="">
-                <a href="/">Tutorite</a>
-                <ul className="navbar-links">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </div>
+            <a href="/">Tutorite</a>
+          </div>
+          <div className="" id="">
 
-        </nav>
-        
-        </>
-
-        
-    );
+            <ul className="flex ">
+              <li>
+                <a href="/home">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/services">Services</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
