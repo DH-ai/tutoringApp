73b970cf89d2844757d12c4c05f66046f4e824ce
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-30 20:46:54 +0530

    ahhhh

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,11 +1,11 @@
-// add to the top
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
 
-const App = () => {
-  return <h1>Hello World!</h1>;
-};
-// modify the createRoot call, delete "ReactDOM"
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+function App() {
+    return (
+        <div>
+            <h1>Hello, Vite + React!</h1>
+        </div>
+    );
+}
+
+export default App;
