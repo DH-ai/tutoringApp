cea43bdfefcb25c7c67d04cde8ce7f31fc27d980
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-31 15:55:00 +0530

    working on the user authentication and login api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'sessions',
+    'server',
+    'webchatapi',
+
 ]
 
 MIDDLEWARE = [
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class auth(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
