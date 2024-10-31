f47a016972313b7af321a4c38a1ecf322dc76b79
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-31 18:17:55 +0530

    working on user authentication

--- backend/users/models.py
+++ backend/users/models.py
@@ -13,8 +13,7 @@ class auth(models.Model):
     state = models.CharField(max_length=50)
     zip = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.username
