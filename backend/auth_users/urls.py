021d89fb9117bf719e34603f319311e9a4fd6766
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-11-07 01:14:08 +0530

    users api completed

--- backend/auth_users/urls.py
+++ backend/auth_users/urls.py
@@ -8,15 +8,19 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('profile/<uuid:id>/',
          UserProfileView.as_view(), name='user-profile-id'),
+    path('profile/update', UserProfileUpdateView.as_view(),
+         name='user-profile-update',),
+    path('profile/delete', UserDeleteView.as_view(),
+         name='user-profile-delete',),
+    
+    
     path('register/', UserRegisterView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
 
-    path('profile/', UserProfileUpdateView.as_view(),
-         name='user-profile-update',),
-    path('profile/', UserDeleteView.as_view(),
-         name='user-profile-delete',),
+    
     path('teachers/', TeacherListView.as_view(), name='teacher-list'),
+    
     path('refresh/', TokenRefreshView.as_view(),
          name='token_refresh', ),
 ]
