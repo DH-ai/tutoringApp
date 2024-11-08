11b8c39dcdc1ee34a604ca633f0c5aca7118bb76
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-11-08 10:29:31 +0530

    deploy once more

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s4=o%8vo*(!-o*^xfr5ore_%5s=-q+wm()b-rm7@*i0hlpo_p$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 
